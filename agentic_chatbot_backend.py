560b983c94d7289717b04743ff67225a159ec60e
Sharksurya006 <suryas632003@gmail.com>
2026-08-03 17:28:49 +0530

    the basic chatbot has been created with persistence memory

--- agentic_chatbot_backend.py
+++ agentic_chatbot_backend.py
@@ -3,9 +3,51 @@ from typing import TypedDict,Annotated
 from langchain_core.messages import BaseMessage, HumanMessage
 from langchain_groq import ChatGroq
 from dotenv import load_dotenv
+from langgraph.graph.message import add_messages
 import os
 from langgraph.checkpoint.memory import MemorySaver
 
 load_dotenv()
 
-llm = ChatGroq(model="")
+os.environ["GROQ_API_KEY"] = os.getenv("GROQ_API_KEY")
+os.environ["GOOGLE_API_KEY"] = os.getenv("GOOGLE_API_KEY")
+
+llm = ChatGroq(model="llama-3.3-70b-versatile", temperature=0.3)
+
+class ChatState(TypedDict):
+	messages: Annotated[list[BaseMessage], add_messages]
+
+
+def chat_node(state: ChatState):
+
+	messages = state['messages']
+
+	response = llm.invoke(messages)
+
+	return {'messages' : [response]}
+
+
+checkpoint = MemorySaver()
+
+graph = StateGraph(ChatState)
+
+# adding the node to the graph
+graph.add_node("chat_node", chat_node)
+
+#adding the edges to the graph
+graph.add_edge(START, 'chat_node')
+graph.add_edge('chat_node', END)
+
+chatbot = graph.compile(checkpointer=checkpoint)
+
+thread_id = '1'
+
+config = {'configurable' : {'thread_id' : thread_id}}
+
+initial_state = {
+	'messages' : [HumanMessage(content = 'my name is Surya?')]
+}
+
+response = chatbot.invoke(initial_state, config= config)
+
+print(response['messages'][-1].content)
--- main.py
+++ main.py
@@ -1,6 +1,41 @@
-def main():
-    print("Hello from agentic-chatbot!")
+from agentic_chatbot_backend import chatbot
+from langchain_core.messages import HumanMessage
+
+import streamlit as st
+
+st.title("Agentic ChatBot")
+
+thread_id = "surya"
+
+config = {'configurable' : {'thread_id' : thread_id}}
+
+if 'message_history' not in st.session_state:
+    st.session_state['message_history'] = []
+ 
+
+for message in st.session_state['message_history']:
+    with st.chat_message(message['role']):
+        st.text(message['content'])
 
 
-if __name__ == "__main__":
-    main()
+user_input = st.chat_input("Enter the message: ")
+
+if user_input:
+    with st.chat_message("user"):
+        # adding the message to the message history
+        st.session_state['message_history'].append({'role' : 'user', 'content' : user_input})
+        st.text(user_input)
+
+    response = chatbot.invoke(
+        {
+            "messages": [
+                HumanMessage(content=user_input)
+            ]
+        },
+        config=config
+    )
+
+    ai_message = response["messages"][-1].content
+    st.session_state['message_history'].append({'role' : 'assistant', 'content' : ai_message})
+    with st.chat_message("assistant"):
+        st.text(ai_message)
